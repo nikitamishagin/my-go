8362e70f128fc6928e18e8c378637df85d3231ea
n.mishagin <n.mishagin@rambler-co.ru>
2023-09-26 02:21:15 +0400

    Add json

--- main.go
+++ main.go
@@ -9,50 +9,89 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+const (
+	redisEndPoint = "localhost:6379"
+	redisPassword = "123456"
+	redisDB       = 0
+)
+
 func main() {
+	// Set subcommand flags
 	setCmd := flag.NewFlagSet("set", flag.ExitOnError)
-	redisEndPoint := setCmd.String("endpoint", "localhost:6379", "Redis server address")
-	redisPassword := setCmd.String("password", "", "Redis password")
-	redisDB := setCmd.Int("db", 0, "Redis data base number")
-	key := setCmd.String("key", "", "Your key")
-	value := setCmd.String("value", "", "Value of your key")
+	setKey := setCmd.String("key", "", "Your key")
+	setValue := setCmd.String("value", "", "Value of your key")
+
+	// Json subcommand flags
+	jsonCmd := flag.NewFlagSet("json", flag.ExitOnError)
+	jsonKey := jsonCmd.String("key", "", "Your key")
+	jsonFilename := jsonCmd.String("data", "", "Path to json data file")
+
+	// Get subcommand flags
+	getCmd := flag.NewFlagSet("get", flag.ExitOnError)
+	getKey := getCmd.String("key", "", "Your key")
 
 	if len(os.Args) < 2 {
 		fmt.Println("error: there is no subcommand")
 		os.Exit(1)
 	}
 
-	setCmd.Parse(os.Args[2:])
-
-	if *key == "" {
-		fmt.Println("error: there is no key")
-		os.Exit(1)
-	}
-
 	client := redis.NewClient(&redis.Options{
-		Addr:     *redisEndPoint,
-		Password: *redisPassword,
-		DB:       *redisDB,
+		Addr:     redisEndPoint,
+		Password: redisPassword,
+		DB:       redisDB,
 	})
 
 	switch os.Args[1] {
 	case "set":
+		setCmd.Parse(os.Args[2:])
+		if *setKey == "" {
+			fmt.Println("error: there is no key")
+			os.Exit(1)
+		}
+
 		ctx := context.Background()
 
-		err := client.Set(ctx, *key, *value, 0).Err()
+		err := client.Set(ctx, *setKey, *setValue, 0).Err()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Your data has been successfully written to Redis")
+	case "json":
+		jsonCmd.Parse(os.Args[2:])
+		if *jsonKey == "" {
+			fmt.Println("error: there is no key")
+			os.Exit(1)
+		}
+
+		content, err := os.ReadFile(*jsonFilename)
+		if err != nil {
+			fmt.Println("error: file opening failed")
+		}
+
+		ctx := context.Background()
+
+		err = client.Set(ctx, *jsonKey, content, 0).Err()
 		if err != nil {
 			panic(err)
 		}
 
 		fmt.Println("Your data has been successfully written to Redis")
 	case "get":
+		getCmd.Parse(os.Args[2:])
+		if *getKey == "" {
+			fmt.Println("error: there is no key")
+			os.Exit(1)
+		}
+
 		ctx := context.Background()
 
-		result, err := client.Get(ctx, *key).Result()
+		value, err := client.Get(ctx, *getKey).Result()
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("Value: %s\n", result)
+
+		fmt.Printf("\"%s\": \"%s\"\n", *getKey, value)
 	default:
 		fmt.Println("error: unknow subcommand")
 		os.Exit(1)
